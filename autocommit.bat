453afdfa2a82d3722c5899ba495c4abd7129c966
phamkhactuy <pktsspktss@gmail.com>
2018-03-12 17:09:53 +0700

    Auto-committed on 12/03/2018

--- autocommit.bat
+++ autocommit.bat
@@ -5,17 +5,7 @@ cd "curl"
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin"
 
-:P
-set ACTION=
-set /P ACTION=Action: %=%
-if "%ACTION%"=="c" (
-  %GIT_PATH% add -A
-	%GIT_PATH% commit -am "Auto-committed on %date%"
-	%GIT_PATH% pull %BRANCH%
-	%GIT_PATH% push %BRANCH%
-)
-if "%ACTION%"=="u" (
-	%GIT_PATH% pull %BRANCH%
-)
-if "%ACTION%"=="exit" exit /b
-goto P
+%GIT_PATH% add -A
+%GIT_PATH% commit -am "Auto-committed on %date%"
+%GIT_PATH% pull %BRANCH%
+%GIT_PATH% push %BRANCH%
