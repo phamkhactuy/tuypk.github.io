e9003509aedb281f68fb2ed46db28fcfe7947bf3
phamkhactuy <pktsspktss@gmail.com>
2018-03-15 14:45:04 +0700

    Auto-committed on

--- autocommit.bat
+++ autocommit.bat
@@ -3,8 +3,9 @@ cd "C:\Users\pktuy\Downloads\PKTUY\GITHUB\tuypk.github.io"
 
 set GIT_PATH="C:\Program Files\Git\bin\git.exe"
 set BRANCH = "origin"
+set Timestamp = %date:~6,4%-%date:~3,2%-%date:~0,2%-%time:~0,2%-%time:~3,2%-%time:~6,2%
 
 %GIT_PATH% add -A
-%GIT_PATH% commit -am "Auto-committed on %date%"
+%GIT_PATH% commit -am "Auto-committed on %Timestamp%"
 %GIT_PATH% pull %BRANCH%
 %GIT_PATH% push %BRANCH%
--- temp.md
+++ temp.md
@@ -1,3 +1,9 @@
 [Quay lại trang chủ](https://phamkhactuy.github.io/tuypk.github.io/index.html)
 
+
+
+
+
+
+
 [Quay lại trang chủ](https://phamkhactuy.github.io/tuypk.github.io/index.html)
