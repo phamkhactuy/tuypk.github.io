f9abb8e0ffd6d7fa383785ea39080ab35ec456c1
phamkhactuy <pktsspktss@gmail.com>
2018-07-06 13:45:05 +0700

    Auto-committed on

--- Nodejs/englishsentence/index.js
+++ Nodejs/englishsentence/index.js
@@ -6,9 +6,43 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var arr3 = [ 'Con meo', 'meo meo', 
+'Con cho' ,'gau gau',
+'Con ga' ,'Cuc tac',
+'Con vit' ,'Quac quac',
+'Con lon' ,'Un in'
+];
+console.log(arr3.length);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }  
+var x=0;
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
-      console.log('message: ' + msg);
+        
+        if(msg=='1')
+        {
+            x=getRandomInt(9);
+            io.emit('chat message', arr3[x]);
+        }
+        else
+            {
+                var y=0;
+                if(x%2 == 0)
+                    y=x+1;
+                else
+                    y=x-1;
+                console.log(msg);                  
+                console.log(arr3[y]);    
+                if(msg==arr3[y])
+                {
+                    io.emit('chat message', 'Dung roi: '+ arr3[y])
+                }
+                else
+                {
+                    io.emit('chat message', 'Sai roi, xin moi viet lai');
+                }
+            }
     });
   });
       
