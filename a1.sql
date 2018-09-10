7c691d49dd1d90db21e3cdfc1ed74ef4d9d4a563
phamkhactuy <pktsspktss@gmail.com>
2018-09-10 14:54:54 +0700

    Update a1.sql

--- a1.sql
+++ a1.sql
@@ -2,7 +2,7 @@
   procedure Vn_extract_inactive_subs(p_datdeb   varchar2,
                                    p_datfin   varchar2,
                                    p_duration varchar2,
-                                   p_cardtype varchar2,
+                                   p_cardsdsdstype varchar2,
                                    p_cstat    varchar2,
                                    p_email    varchar2,
                                    P_numproc  usr_procedure.numproc%type) is
